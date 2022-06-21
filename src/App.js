62b8fb4aa329d3b698537346fdc11a52a5527ff5
stjodas <61805550+stjodas@users.noreply.github.com>
2022-06-21 20:52:28 -0300

    oi

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Bunito from './components/Bunito.js';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Bunito />
     </div>
   );
 }
---
+++ src/components/Bunito.js
@@ -0,0 +1,11 @@
+
+
+function Bunito() {
+  return (
+    <div className="App">
+        <p>Bunitoso</p>
+    </div>
+  );
+}
+
+export default Bunito;
